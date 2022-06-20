1c281d464e2aa5a5a338a6dd08fb4dba5d4c8128
genchadt <62498018+genchadt@users.noreply.github.com>
2022-06-19 22:41:53 -0400

    Tightened up some code

--- improvedlightbulblongevity/Contents/mods/improvedlightbulblongevity/media/lua/client/ImprovedLightBulbLongevity.lua
+++ improvedlightbulblongevity/Contents/mods/improvedlightbulblongevity/media/lua/client/ImprovedLightBulbLongevity.lua
@@ -1,21 +1,17 @@
 require("BulbList.lua")
 
--- Initialize tables we'll need
-if not BulbList then BulbList = {} end
-if not BulbsColorList then BulbsColorList = {} end
-
 function SetBulbConditionMax()
   -- Get bulb lists
-  BulbsList = Bulbs
-  BulbsColorList = BulbsColor
+  if not BulbsList then BulbsList = Bulbs end
+  if not BulbsColorList then BulbsColorList = BulbsColor end
 
   local param = "ConditionMax"
   local bulbs_condition = SandboxVars.ImprovedLightBulbLongevity.LightBulbs_ConditionMax
   local bulbscolor_condition = SandboxVars.ImprovedLightBulbLongevity.LightBulbsColor_ConditionMax
   local item
 
-  -- If data exists, fix each item's ConditionMax value
-  if BulbList ~= nil and (bulbs_condition ~= nil and bulbs_condition ~= 100) then
+  -- If data exists and bulb condition isn't default or nil, fix each item's ConditionMax value
+  if BulbsList ~= nil and (bulbs_condition ~= nil and bulbs_condition ~= 100) then
     for k,v in pairs(BulbsList) do
       item = ScriptManager.instance:getItem(v)
       if item ~= nil then
